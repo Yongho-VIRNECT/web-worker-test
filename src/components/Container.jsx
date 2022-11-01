344dc31cb5ed85d8a9cd143d2683d4f07f8679df
silqwer <58449745+Yongho-VIRNECT@users.noreply.github.com>
2022-11-01 14:02:37 +0900

    feat: 손 추척

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {createWorkerFactory, useWorker} from '@shopify/react-web-worker';
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+
+import {  HAND_CONNECTIONS } from "@mediapipe/hands";
 
 const createWorker = createWorkerFactory(() => import('./worker.js'));
 
@@ -9,6 +12,7 @@ const Container = () => {
   let stream = null;
   
   const webWorker = useWorker(createWorker);
+  const [requestId, setRequestId] = useState(null);
 
   const onClick = async () => {
    const result = await webWorker.hello("silqwer");
@@ -21,6 +25,42 @@ const Container = () => {
     const result2 = await webWorker.handsSend(imageData);
     console.log(result2)
   }
+
+  const canvasRender = async() => {
+    canvasRef.current.getContext("2d").save();
+    canvasRef.current.getContext("2d").clearRect(0, 0, 640, 480);
+    canvasRef.current.getContext("2d").drawImage(videoRef.current, 0, 0, 640, 480);
+
+    const imageData = canvasRef.current.getContext("2d").getImageData(0, 0, 640, 480);
+    const results = await webWorker.handsSend(imageData);
+ 
+
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        drawConnectors(canvasRef.current.getContext("2d"), landmarks, HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5,
+        });
+        drawLandmarks(canvasRef.current.getContext("2d"), landmarks, {
+          color: "#FF0000",
+          lineWidth: 2,
+        });
+      }
+    }
+
+    const id = requestAnimationFrame(canvasRender);
+    setRequestId(id);
+  }
+
+  const onClickStop = () => {
+    const id = cancelAnimationFrame(requestId);
+    setRequestId(id);
+  }
+
+  const onClickPlay = async () => {
+    canvasRender();
+  }
+
 
   const getMedia = async (option) => {
     try {
@@ -48,6 +88,8 @@ const Container = () => {
       <video ref={videoRef} />
       <canvas ref={canvasRef} width={640} height={480} />
       <button type='button' onClick={onClick}>전송</button>
+      <button type='button' onClick={onClickPlay}>재생</button>
+      <button type='button' onClick={onClickStop}>정지</button>
     </>
 
   )
--- src/components/worker.js
+++ src/components/worker.js
@@ -1,18 +1,14 @@
-/* eslint-disable no-undef */
 
-//importScripts("/worker/hands.js?time="+time);
-//import { Hands } from '/worker/hands.js';
-let time = new Date().getTime();
-
+import {  Hands } from "@mediapipe/hands";
 
 let hands = null;
 let handsModelInit = false;
-
+let result = null;
 const initHands = () => {
   hands = new Hands({
     locateFile: (file) => {
       console.log("FILE:",file);
-      return `/worker/${file}`
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
     },
   });
 
@@ -22,37 +18,25 @@ const initHands = () => {
     minDetectionConfidence: 0.5,
     minTrackingConfidence: 0.5,
   });
-  
-  hands.onResults((result)=>{
-    postMessage(result);
+
+  hands.onResults((results)=>{
+    console.log(results);
+    result = results
   });
+  
 
   console.log("holistic worker initialization!");
   handsModelInit = true
 }
 
-//initHands();
-
-// onmessage = async (e) => {
-//   console.log(e.data);
-//   const [eventName, data] = e.data;
-//   if(eventName === "hello"){
-//     postMessage("How are you? :D");
-//     return 
-//   } 
-
-//   if(eventName === "hands"){
-//     console.log(data);
-//     await hands.send({image: data});
-//   }
-// }
+initHands();
 
 export const hello = (name) => {
   return `How are you? ${name} :D`;
 }
 
-export const handsSend = (imageData) => {
-  console.log(imageData)
-  return `imageData :D`;
+export const handsSend = async (imageData) => {
+  await hands.send({ image: imageData });
+  return result;
 }
 
